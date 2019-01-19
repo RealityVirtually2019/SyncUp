ee444eb5b2436676f3286879d9bd9e08d4f057a8
Wendy Wei <wendywei@mit.edu>
2019-01-19 12:10:31 -0500

    Updated with L and R controller position code (not tested irl)

--- SyncMain/Assets/Scripts/Wall/WallCube.cs
+++ SyncMain/Assets/Scripts/Wall/WallCube.cs
@@ -8,35 +8,38 @@ public class WallCube : MonoBehaviour {
     public Color onColor;
     public Material material;
 
-    // Use this for initialization
-    void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Vector3 playerPos = mainPlayer.transform.position;
+    private bool InCube(Vector3 avatarPos)
+    {
         Vector3 cubePos = gameObject.transform.position;
         Vector3 cubeScale = gameObject.transform.lossyScale;
+        return (cubePos.x - 0.5 * cubeScale.x <= avatarPos.x) &&
+            (avatarPos.x <= cubePos.x + 0.5 * cubeScale.x) &&
+            (cubePos.y - 0.5 * cubeScale.y <= avatarPos.y) &&
+            (avatarPos.y <= cubePos.y + 0.5 * cubeScale.y);
+    }
 
-        Debug.Log("xPos: " + playerPos.x.ToString() + ", yPos: " + playerPos.y.ToString());
-        // Check if player is "in bounds" of cube
-        bool inCube = (cubePos.x - 0.5 * cubeScale.x <= playerPos.x) &&
-            (playerPos.x <= cubePos.x + 0.5 * cubeScale.x) &&
-            (cubePos.y - 0.5 * cubeScale.y <= playerPos.y) &&
-            (playerPos.y <= cubePos.y + 0.5 * cubeScale.y);
+    // Use this for initialization
+    void Start() {
 
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 headPos = mainPlayer.transform.position;
+        Vector3 leftHandPos = mainPlayer.transform.Find("TrackingSpace").transform.TransformPoint(OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTrackedRemote));
+        Vector3 rightHandPos = mainPlayer.transform.Find("TrackingSpace").transform.TransformPoint(OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTrackedRemote));
+        
         MeshRenderer cubeRenderer = gameObject.GetComponent<MeshRenderer>();
 
-        if (inCube) // Change color
+        // Change color
+        if ((InCube(headPos) || InCube(leftHandPos)) || InCube(rightHandPos))
         {
-            Material onMaterial = new Material(material);
-            onMaterial.color = onColor;
+            Material onMaterial = new Material(material){ color = onColor };
             cubeRenderer.material = onMaterial;
         }
         else
         {
-            Material offMaterial = new Material(material);
-            offMaterial.color = material.color;
+            Material offMaterial = new Material(material) { color = material.color };
             cubeRenderer.material = offMaterial;
         }
     }
