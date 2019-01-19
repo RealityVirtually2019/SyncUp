6a7f3a36f3b4a3a5d2f622409f1bbb4049b68e71
Wendy Wei <wendywei@mit.edu>
2019-01-19 13:42:58 -0500

    Added parent script on cubes wall to detect when 3 adjacent cubes create a base tetris block (not tested on headset)

---
+++ SyncMain/Assets/BaseBlocks.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class BaseBlocks : MonoBehaviour {
+    readonly List<GameObject> activeCubes = new List<GameObject>();
+    readonly string activeTag = "active";
+
+    private bool CheckAdjacent(Transform object1, Transform object2, double precision = 1e-4)
+    {
+        // check vertical or horizontal adjacency on grid
+        return (Math.Abs(object1.transform.position.x - object2.transform.position.x) < precision ||
+            Math.Abs(object1.transform.position.y - object2.transform.position.y) < precision);
+    }
+
+    private bool CheckBlock(Transform[] cubes)
+    {
+        // only check if there are 3 active cubes available to make a base block
+        if (cubes.Length == 3)
+        {
+            return ((CheckAdjacent(cubes[0], cubes[1]) && CheckAdjacent(cubes[1], cubes[2])) ||
+                (CheckAdjacent(cubes[0], cubes[1]) && CheckAdjacent(cubes[0], cubes[2]))) ||
+                (CheckAdjacent(cubes[1], cubes[2]) && CheckAdjacent(cubes[0], cubes[2]));
+        }
+        return false;
+    }
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Transform[] arrayOfChildren = gameObject.transform.Cast<Transform>().Where(c => c.gameObject.tag == "active").ToArray();
+        bool foundBlock = CheckBlock(arrayOfChildren);
+    }
+}
--- SyncMain/Assets/Scripts/Wall/WallCube.cs
+++ SyncMain/Assets/Scripts/Wall/WallCube.cs
@@ -26,21 +26,24 @@ public class WallCube : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         Vector3 headPos = mainPlayer.transform.position;
-        Vector3 leftHandPos = mainPlayer.transform.Find("TrackingSpace").transform.TransformPoint(OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTrackedRemote));
-        Vector3 rightHandPos = mainPlayer.transform.Find("TrackingSpace").transform.TransformPoint(OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTrackedRemote));
+        //Vector3 leftHandPos = mainPlayer.transform.Find("TrackingSpace").transform.TransformPoint(OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTrackedRemote));
+        //Vector3 rightHandPos = mainPlayer.transform.Find("TrackingSpace").transform.TransformPoint(OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTrackedRemote));
         
         MeshRenderer cubeRenderer = gameObject.GetComponent<MeshRenderer>();
 
         // Change color
-        if ((InCube(headPos) || InCube(leftHandPos)) || InCube(rightHandPos))
+        if (InCube(headPos))
+        //if ((InCube(headPos) || InCube(leftHandPos)) || InCube(rightHandPos))
         {
             Material onMaterial = new Material(material){ color = onColor };
             cubeRenderer.material = onMaterial;
+            gameObject.tag = "active";
         }
         else
         {
             Material offMaterial = new Material(material) { color = material.color };
             cubeRenderer.material = offMaterial;
+            gameObject.tag = "inactive";
         }
     }
 }
