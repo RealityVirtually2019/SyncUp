b3490dca37851bf707d2c673d2c34760d6b6f379
Wendy Wei <wendywei@mit.edu>
2019-01-18 21:58:42 -0500

    Added tetris combo pieces

---
+++ SyncMain/Assets/Scripts/Blocks/Spawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+    public Transform spawnLocation;
+    public GameObject prefabSpawn;
+	// Use this for initialization
+	void Start () {
+        GameObject spawnedObject = Instantiate(prefabSpawn, spawnLocation.position, spawnLocation.rotation);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
