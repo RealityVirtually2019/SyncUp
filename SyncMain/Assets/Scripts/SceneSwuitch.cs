cca3578f539b75a30e7a366fe0ea33f2b44a3283
Danny Saetern <kdsaetern@gmail.com>
2019-01-20 12:58:51 -0500

    sceneSolo2 sent

---
+++ SyncMain/Assets/Scripts/SceneSwuitch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwuitch : MonoBehaviour {
+
+    public OVRScreenFade fader;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("hand"))
+        {
+            StartCoroutine(StartLogin(1.5f));
+        }
+    }
+
+    IEnumerator StartLogin(float time)
+    {
+        fader.FadeOut();
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(2);
+    }
+}
